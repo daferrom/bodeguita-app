5536816ee3964bcb71b7baeed4b6896ab963f887
daferrom <daferrom@gmail.com>
2021-09-06 13:06:12 -0500

    developing component Cardin procress

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Card.css'
 
 const Card = () => {
     return (
-        <div>
+        <div className ="cardContainer">
             <h3 className="productTitle">
                 Combo pa' desayunar
             </h3>
