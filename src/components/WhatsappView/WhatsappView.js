8036239a8b3145c0a7430d72165648e8f99ef8c3
daferrom <daferrom@gmail.com>
2021-09-10 09:26:17 -0500

    initial point Shopping cart my version

---
+++ src/components/WhatsappView/WhatsappView.js
@@ -0,0 +1,3 @@
+const url = `https://api.whatsapp.com/send?phone=${phoneWa}&text=
+        Hola, ${name} ${lastName} Bienvenido a Milacam! 🐾 %0ATus productos son:%0A══════════════════%0A${listText}══════════════════%0ADirección: ${addres}%0ABarrio: ${neighborhood}%0ACiudad: ${city}%0ACelular: ${phone}%0A%0A*TOTAL:* $${totalCart}`;
+Esa es la api de WhatsApp
